98c2122cac776af70fd0c9360e432e19937a9bc7
Marcin8013 <marcing2067@wp.pl>
2021-12-15 16:48:02 +0100

    Fix home page(shapes covered nav links)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: '', component: HomeComponent }]),
+    HomeRoutingModule,
     SharedModule,
   ],
 })
