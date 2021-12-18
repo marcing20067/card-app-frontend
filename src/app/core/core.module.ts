496760074ba3037b887561b3d0f38ec271e153f7
Marcin8013 <marcing2067@wp.pl>
2021-12-18 16:32:05 +0100

    Add core page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from '../auth/auth.interceptor';
+import { CoreComponent } from './core.component';
+import { LayoutModule } from '../layout/layout.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoreComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
+    LayoutModule
   ],
-  exports: [],
+  exports: [CoreComponent],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
 })
 export class CoreModule { }
