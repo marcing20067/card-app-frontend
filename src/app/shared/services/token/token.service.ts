e37213be6793b22aa289140b688b7e1557452055
Marcin8013 <marcing2067@wp.pl>
2022-01-22 12:06:20 +0100

    Delete camel case in files
    Command for finding camelCase: find ./src -type f -regex '.*[a-z][A-Z].*'

--- src/app/shared/services/token/token.service.ts
+++ src/app/shared/services/token/token.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
-import { TokenData } from '../../models/tokenData.model';
+import { TokenData } from '../../models/token-data.model';
 
 @Injectable({
   providedIn: 'root',
